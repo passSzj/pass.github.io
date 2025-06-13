1f0edfa7e76e068dc68b125376ce3e8fc0b3572d
passSzj <y462477976@outlook.com>
2025-06-13 20:18:57 +0800

    Site updated: 2025-06-13 20:18:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/13/hello-world/","2025/02/13/Context/","2024/05/01/摄影日记/"];function toRandomPost(){
+var posts=["2025/02/13/Context/","2024/05/01/摄影日记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

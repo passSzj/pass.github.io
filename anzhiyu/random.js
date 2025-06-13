a138de0150bcc401b44e7670eb4cfdb52f283217
passSzj <y462477976@outlook.com>
2025-06-13 16:29:05 +0800

    Site updated: 2025-06-13 16:29:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/13/hello-world/","2025/06/13/Context/"];function toRandomPost(){
+var posts=["2025/06/13/hello-world/","2025/06/13/Context/","2025/06/13/摄影日记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
